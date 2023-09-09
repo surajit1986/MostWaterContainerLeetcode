5fd024288e790a196575b70b27fa0b2e610ed176
Surajit Ghosh <Surajit.1986@gmail.com>
2023-09-09 10:03:16 +0530

    Most water container problem in leetcode

---
+++ MostWaterContainerLeetcode/MostWaterContainerLeetcode/MaxArea.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MostWaterProblem
+{
+    public class MaxArea
+    {
+        public int GetMaxArea(int[] heights)
+        {
+            int maxArea = 0;
+            int height = 0;
+            int length = 0;
+            int area = 0;
+
+            for(int i =0; i < heights.Length; i++)
+            {
+                for(int j =i+1;j<heights.Length; j++)
+                {
+                    height = Math.Min(heights[i], heights[j]);
+                    length = j-i;
+                    area = height * length;
+                    maxArea = Math.Max(maxArea, area);
+                }
+            }
+
+            return maxArea;
+        }
+
+        //Optimized Solution :
+        public int GetMaxAreaOptimized(int[] heights)
+        {
+            int maxArea = 0;
+            int start = 0;
+            int end = heights.Length-1;
+            int area = 0;
+            int height = 0;
+            int width = 0;
+
+            while(start < end)
+            {
+                height = Math.Min(heights[start], heights[end]);
+                width = end - start;
+                area = height * width;
+                maxArea = Math.Max(area, maxArea);
+
+                if (heights[start] < heights[end])
+                {
+                    start++;
+                }
+                else
+                {
+                    end--;
+                }
+            }
+
+            return maxArea;
+        }
+    }
+}
---
+++ MostWaterContainerLeetcode/MostWaterContainerLeetcode/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+
+
+//You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and(i, height[i]).
+
+//Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+//Return the maximum amount of water a container can store.
+
+//Notice that you may not slant the container.
+
+//using static System.Collections.Specialized.BitVector32;
+
+//Input: height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+//Output: 49
+//Explanation: The above vertical lines are represented by array [1, 8, 6, 2, 5, 4, 8, 3, 7].In this case, the max area of water(blue section) the container can contain is 49.
+//Example 2:
+
+//Input: height = [1, 1]
+//Output: 1
+
+
+using MostWaterProblem;
+
+int[] heights = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+MaxArea area = new MaxArea();
+Console.WriteLine($"maximum area water can contain:{area.GetMaxArea(heights)}");
+Console.WriteLine($"maximum area water can contain(optimized):{area.GetMaxAreaOptimized(heights)}");
+Console.ReadKey();
